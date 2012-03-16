76076a3d54684e93ae1bb32f5493974e65392f15
Wenyuan Chi <wenyuan_chi@htc.com>
2012-03-16 13:31:12 +0800

    add big5 table

---
+++ encodings/table/gen.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+// BIG5
+var cp950_b2u = { host:'moztw.org',path:'/docs/big5/table/cp950-b2u.txt'},
+    cp950_u2b = { host:'moztw.org',path:'/docs/big5/table/cp950-u2b.txt'}
+
+http.get(cp950_b2u, function(res) {
+  var data = '';
+  res.on('data', function(chunk) {
+    data += chunk;
+  });
+  res.on('end', function() {
+    var table = {};
+    data = data.split('\n').slice(1);
+    data.forEach(function(line, idx) {
+      var pair = line.split(' ');
+      var key = parseInt(pair[0]);
+      var val = parseInt(pair[1]);
+      table[key] = val;
+    });
+    var ws = fs.createWriteStream('cp950.js');
+    ws.write('module.exports=');
+    ws.end(JSON.stringify(table));
+  });
+});
