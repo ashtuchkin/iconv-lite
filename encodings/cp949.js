c625476e9bd01e7d64c4f89265f397dca40d3480
Dan Gibson <dan@gandale.com>
2013-08-08 18:29:34 +1000

    Added a test to compare iconv-lite with iconv for CP949

--- encodings/cp949.js
+++ encodings/cp949.js
@@ -1,26 +1,28 @@
-var cp949Table = {};
-
 var fs = require('fs');
-var f = fs.readFileSync(require.resolve('./table/cp949.txt'), 'utf8');
-var i;
-var line;
-var lines = f.split('\n');
-var col;
-for (i=0; i<lines.length; i++)
-{
-	line = lines[i];
-	if (line.charAt(0) !== '#')
+
+function loadTable() {
+	var cp949Table = {};
+	var f = fs.readFileSync(require.resolve('./table/cp949.txt'), 'utf8');
+	var i;
+	var line;
+	var lines = f.split('\n');
+	var col;
+	for (i=0; i<lines.length; i++)
 	{
-		col = line.split('\t');
-		cp949Table[parseInt(col[0],16)] = parseInt(col[1],16);
-		//cp949Table[parseInt(col[0],16)] = 51; // 3
+		line = lines[i];
+		if ((line) && (line.charAt(0) !== '#'))
+		{
+			col = line.split('\t');
+			if (col[1].trim() !== "") cp949Table[parseInt(col[0],16)] = parseInt(col[1],16);
+		}
 	}
+	return cp949Table;
 }
 
 module.exports = {
 	'ks_c_56011987': 'cp949',
 	'cp949': {
 		type: 'table',
-		table: cp949Table
+		table: loadTable()
 	}
 }
--- test/cp949-test.js
+++ test/cp949-test.js
@@ -1,18 +1,64 @@
-var vows    = require('vows'),
-    assert  = require('assert'),
-    iconv   = require(__dirname+'/../');
+var vows    = require("vows"),
+	assert  = require("assert"),
+	fs      = require("fs"),
+	iconv   = require(__dirname+"/../");
 
 var testStringUtf = "\u00ba\u00b0\uc96fabc",
-    testStringCP949Buffer = new Buffer([0xa8,0xac, 0xa1,0xc6, 0xa2,0xa0, 0x61, 0x62, 0x63]);
+	testStringCP949Buffer = new Buffer([0xa8, 0xac, 0xa1,0xc6, 0xa2,0xa0, 0x61, 0x62, 0x63]);
+
+// Returns an object with a buffer (other) and a string (utf8) that contain every character
+// We use this string/buffer to compare iconv-lite with iconv
+function getBigText() {
+	var utf8 = "";
+	var other = [];
+	var f = fs.readFileSync(require.resolve("../encodings/table/cp949.txt"), "utf8");
+	var i;
+	var line;
+	var lines = f.split("\n");
+	var col;
+	var x;
+	for (i=0; i<lines.length; i++)
+	{
+		line = lines[i];
+		if ((line) && (line.charAt(0) !== "#"))
+		{
+			col = line.split("\t");
+			if (col[1].trim() !== "")
+			{
+				x = parseInt(col[0],16);
+				if (x > 127) other.push((x >> 8) & 0xff);
+				other.push(x & 0xff);
+	
+				x = parseInt(col[1],16);
+				utf8 += String.fromCharCode(x);
+			}
+		}
+	}
+	var p = {};
+	p.utf8 = utf8;
+	p.other = new Buffer(other);
+	return p;
+}
 
 vows.describe("CP949 tests").addBatch({
-    "Vows is working": function() {},
-    "CP949 correctly encoded/decoded": function() {    
-        assert.strictEqual(iconv.toEncoding(testStringUtf, "cp949").toString('binary'), testStringCP949Buffer.toString('binary'));
-        assert.strictEqual(iconv.fromEncoding(testStringCP949Buffer, "cp949"), testStringUtf);
-    },
-    "ks_c_5601-1987 correctly encoded/decoded": function() {    
-        assert.strictEqual(iconv.toEncoding(testStringUtf, "ks_c_5601-1987").toString('binary'), testStringCP949Buffer.toString('binary'));
-        assert.strictEqual(iconv.fromEncoding(testStringCP949Buffer, "ks_c_5601-1987"), testStringUtf);
-    },
+	"Vows is working": function() {},
+	"CP949 correctly encoded/decoded": function() {
+		assert.strictEqual(iconv.toEncoding(testStringUtf, "cp949").toString("binary"), testStringCP949Buffer.toString("binary"));
+		assert.strictEqual(iconv.fromEncoding(testStringCP949Buffer, "cp949"), testStringUtf);
+	},
+	"ks_c_5601-1987 correctly encoded/decoded": function() {
+		assert.strictEqual(iconv.toEncoding(testStringUtf, "ks_c_5601-1987").toString("binary"), testStringCP949Buffer.toString("binary"));
+		assert.strictEqual(iconv.fromEncoding(testStringCP949Buffer, "ks_c_5601-1987"), testStringUtf);
+	},
+
+	"compare with iconv result": function() {
+		var p = getBigText();
+
+		// compare iconv-lite with utf8
+		assert.strictEqual(iconv.fromEncoding(p.other, "cp949").toString(), p.utf8);
+
+		// compare iconv with utf8
+		var iconvc = new (require("iconv").Iconv)("cp949", "utf8");
+		assert.strictEqual(iconvc.convert(p.other).toString(), p.utf8);
+	},
 }).export(module)
