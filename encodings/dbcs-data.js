e731fbe8164537fab14f00b78ef87e99e79a1ed1
Alexander Shtuchkin <ashtuchkin@gmail.com>
2014-04-21 00:50:38 -0700

    Added docs for japanese encodings.

--- encodings/dbcs-data.js
+++ encodings/dbcs-data.js
@@ -5,13 +5,34 @@
 module.exports = {
     
     // == Japanese/ShiftJIS ====================================================
-    // CP932 is an extension of Shift_JIS (JIS 0208) with ASCII bottom
-    'windows932': 'cp932',
-    '932': 'cp932',
-    'cp932': {
-        type: '_dbcs',
-        table: ['./tables/shiftjis.json', './tables/cp932-added.json'],
-    },
+    // All japanese encodings are based on JIS X set of standards:
+    // JIS X 0201 - Single-byte encoding of ASCII + ¥ + Kana chars at 0xA1-0xDF.
+    // JIS X 0208 - Main set of 6879 characters, placed in 94x94 plane, to be encoded by 2 bytes. 
+    //              Has several variations in 1978, 1983, 1990 and 1997.
+    // JIS X 0212 - Supplementary plane of 6067 chars in 94x94 plane. 1990. Effectively dead.
+    // JIS X 0213 - Extension and modern replacement of 0208 and 0212. Total chars: 11233.
+    //              2 planes, first is superset of 0208, second - revised 0212.
+    //              Introduced in 2000, revised 2004. Some characters are in Unicode Plane 2 (0x2xxxx)
+
+    // Byte encodings are:
+    //  * Shift_JIS: Compatible with 0201, uses not defined chars in top half as lead bytes for double-byte
+    //               encoding of 0208. Lead byte ranges: 0x81-0x9F, 0xE0-0xEF; Trail byte ranges: 0x40-0x7E, 0x80-0x9E, 0x9F-0xFC.
+    //               Windows CP932 is a superset of Shift_JIS. Some companies added more chars, notably KDDI.
+    //  * EUC-JP:    Up to 3 bytes per character. Used mostly on *nixes.
+    //               0x00-0x7F       - lower part of 0201
+    //               0x8E, 0xA1-0xDF - upper part of 0201
+    //               (0xA1-0xFE)x2   - 0208 plane (94x94).
+    //               0x8F, (0xA1-0xFE)x2 - 0212 plane (94x94).
+    //  * JIS X 208: 7-bit, direct encoding of 0208. Byte ranges: 0x21-0x7E (94 values). Uncommon.
+    //               Used as-is in ISO2022 family.
+    //  * ISO2022-JP: Stateful encoding, with escape sequences to switch between ASCII, 
+    //                0201-1976 Roman, 0208-1978, 0208-1983.
+    //  * ISO2022-JP-1: Adds esc seq for 0212-1990.
+    //  * ISO2022-JP-2: Adds esc seq for GB2313-1980, KSX1001-1992, ISO8859-1, ISO8859-7.
+    //  * ISO2022-JP-3: Adds esc seq for 0201-1976 Kana set, 0213-2000 Planes 1, 2.
+    //  * ISO2022-JP-2004: Adds 0213-2004 Plane 1.
+    //
+    // After JIS X 0213 appeared, Shift_JIS-2004, EUC-JISX0213 and ISO2022-JP-2004 followed, with just changing the planes.
 
 
     'shiftjis': {
@@ -20,6 +41,14 @@ module.exports = {
     },
     'csshiftjis': 'shiftjis',
     'mskanji': 'shiftjis',
+
+    // CP932 is an extension of Shift_JIS.
+    'windows932': 'cp932',
+    '932': 'cp932',
+    'cp932': {
+        type: '_dbcs',
+        table: ['./tables/shiftjis.json', './tables/cp932-added.json'],
+    },
 
     // TODO: KDDI extension to Shift_JIS
     // TODO: IBM CCSID 942 = CP932, but F0-F9 custom chars and other char changes.
