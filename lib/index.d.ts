3c028e2436026507287fb21b46bf2996f012ca1f
Lars Støttrup Nielsen <larsstoettrup@gmail.com>
2017-03-27 17:10:24 +0200

    Added requirement text to header

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,8 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  REQUIREMENT: This definition is dependant on the @types/node definition.
+ *  Install with `npm install @types/node --save-dev`
  *--------------------------------------------------------------------------------------------*/
 
 declare module 'iconv-lite' {
