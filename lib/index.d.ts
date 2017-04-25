f8698995923541d33459be1215d4b4baf17a1837
Lars Støttrup Nielsen <larsstoettrup@gmail.com>
2017-04-25 16:41:35 +0200

    Added requested changes
    - options no longer "any"
    - removed reference to license.txt, people can look up the MIT license online.
    - designated interface fields as optional

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,14 +1,14 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *  REQUIREMENT: This definition is dependant on the @types/node definition.
+ *  Licensed under the MIT License.
+ *  REQUIREMENT: This definition is dependent on the @types/node definition.
  *  Install with `npm install @types/node --save-dev`
  *--------------------------------------------------------------------------------------------*/
 
 declare module 'iconv-lite' {
-	export function decode(buffer: NodeBuffer, encoding: string, options?: any): string;
+	export function decode(buffer: NodeBuffer, encoding: string, options?: Options): string;
 
-	export function encode(content: string, encoding: string, options?: any): NodeBuffer;
+	export function encode(content: string, encoding: string, options?: Options): NodeBuffer;
 
 	export function encodingExists(encoding: string): boolean;
 
@@ -18,7 +18,7 @@ declare module 'iconv-lite' {
 }
 
 export interface Options {
-    stripBOM: boolean;
-    addBOM: boolean;
-    defaultEncoding: string;
+    stripBOM?: boolean;
+    addBOM?: boolean;
+    defaultEncoding?: string;
 }
