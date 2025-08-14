318acccf84ed3d7c2737b7ef8b14f0c7cba97941
Paul Ebose <paulebose@gmail.com>
2025-08-14 21:07:36 +0100

    add more exhaustive lists of supported encodings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -8,26 +8,35 @@
 
 // --- Unicode Encodings ---
 type UnicodeEncoding =
-  | "UTF7"
-  | "UTF7-IMAP"
-  | "UTF-16BE"
-  | "UTF-16" // With BOM
-  | "UCS-4"
-  | "UTF-32" // With BOM, alias for UCS-4
-  | "UTF-32LE"
-  | "UTF-32BE";
+  | "utf7"
+  | "utf7imap"
+  | "utf16be"
+  | "utf16" // With BOM
+  | "ucs4"
+  | "utf32" // With BOM, alias for UCS-4
+  | "utf32le"
+  | "utf32be";
 
 // --- Single-byte Encodings ---
 
 // Helper type for Windows codepage numbers
 type WindowsCodepageNumber = "874" | "1250" | "1251" | "1252" | "1253" | "1254" | "1255" | "1256" | "1257" | "1258";
 
-// Windows codepages with aliases (winXXX, cpXXX, XXX)
+// Windows codepages with aliases (windows-XXX, winXXX, cpXXX, XXX)
 type WindowsCodepage =
   | WindowsCodepageNumber
   | `windows-${WindowsCodepageNumber}`
+  | `windows${WindowsCodepageNumber}`
   | `win${WindowsCodepageNumber}`
-  | `cp${WindowsCodepageNumber}`;
+  | `cp${WindowsCodepageNumber}`
+  | "msee"
+  | "mscyrl"
+  | "msansi"
+  | "msgreek"
+  | "msturk"
+  | "mshebr"
+  | "msarab"
+  | "winbaltrim";
 
 // Helper type for ISO codepage numbers
 type ISOCodepageNumber = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "13" | "14" | "15" | "16";
@@ -49,13 +58,83 @@ type ISOCodepageAliasNumber =
   | "28606";
 
 // ISO codepages with aliases
-type ISOCodepage = `iso-8859-${ISOCodepageNumber}` | ISOCodepageAliasNumber | `cp${ISOCodepageAliasNumber}`;
+type ISOCodepage =
+  | `iso-8859-${ISOCodepageNumber}`
+  | `iso8859${ISOCodepageNumber}`
+  | ISOCodepageAliasNumber
+  | `cp${ISOCodepageAliasNumber}`
+  | "latin1"
+  | "latin2"
+  | "latin3"
+  | "latin4"
+  | "latin5"
+  | "latin6"
+  | "latin7"
+  | "latin8"
+  | "latin9"
+  | "latin10"
+  | "csisolatin1"
+  | "csisolatin2"
+  | "csisolatin3"
+  | "csisolatin4"
+  | "csisolatincyrillic"
+  | "csisolatinarabic"
+  | "csisolatingreek"
+  | "csisolatinhebrew"
+  | "csisolatin5"
+  | "csisolatin6"
+  | "l1"
+  | "l2"
+  | "l3"
+  | "l4"
+  | "l5"
+  | "l6"
+  | "l7"
+  | "l8"
+  | "l9"
+  | "l10"
+  | "isoir100"
+  | "isoir101"
+  | "isoir109"
+  | "isoir110"
+  | "isoir144"
+  | "isoir127"
+  | "isoir126"
+  | "isoir138"
+  | "isoir148"
+  | "isoir157"
+  | "isoir179"
+  | "isoir199"
+  | "isoir203"
+  | "isoir226"
+  | "cp819"
+  | "ibm819"
+  | "cyrillic"
+  | "arabic"
+  | "arabic8"
+  | "ecma114"
+  | "asmo708"
+  | "greek"
+  | "greek8"
+  | "ecma118"
+  | "elot928"
+  | "hebrew"
+  | "hebrew8"
+  | "turkish"
+  | "turkish8"
+  | "thai"
+  | "thai8"
+  | "celtic"
+  | "celtic8"
+  | "isoceltic";
 
 // Helper type for IBM codepage numbers
 type IBMCodepageNumber =
   | "437"
+  | "720"
   | "737"
   | "775"
+  | "808"
   | "850"
   | "852"
   | "855"
@@ -83,39 +162,99 @@ type IBMCodepageNumber =
 // IBM codepages with aliases (ibmXXX, csibmXXX, XXX)
 type IBMCodepage =
   | IBMCodepageNumber
-  | `CP${IBMCodepageNumber}`
+  | `cp${IBMCodepageNumber}`
   | `ibm${IBMCodepageNumber}`
-  | `csibm${IBMCodepageNumber}`;
+  | `csibm${IBMCodepageNumber}`
+  | "cspc8codepage437"
+  | "cspc775baltic"
+  | "cspc850multilingual"
+  | "cspcp852"
+  | "cspc862latinhebrew"
+  | "cpgr";
 
 // Mac codepages
 type MacCodepage =
-  | "macCroatian"
-  | "macCyrillic"
-  | "macGreek"
-  | "macIceland"
-  | "macRoman"
-  | "macRomania"
-  | "macThai"
-  | "macTurkish"
-  | "macUkraine"
-  | "macintosh"; // Alias for macRoman
+  | "maccroatian"
+  | "maccyrillic"
+  | "macgreek"
+  | "maciceland"
+  | "macroman"
+  | "macromania"
+  | "macthai"
+  | "macturkish"
+  | "macukraine"
+  | "maccenteuro"
+  | "10000"
+  | "10006"
+  | "10007"
+  | "10029"
+  | "10079"
+  | "10081";
 
 // KOI8 codepages
-type KOI8Codepage = "KOI8-R" | "KOI8-U" | "KOI8-RU" | "KOI8-T";
+type KOI8Codepage =
+  | "koi8r"
+  | "koi8u"
+  | "koi8ru"
+  | "koi8t"
+  | "cp20866"
+  | "20866"
+  | "ibm878"
+  | "cskoi8r"
+  | "cp21866"
+  | "21866"
+  | "ibm1168";
 
 // Miscellaneous single-byte encodings
 type MiscellaneousSingleByte =
-  | "ARMSCII-8"
-  | "RK1048"
-  | "TCVN"
-  | "GEORGIAN-ACADEMY"
-  | "GEORGIAN-PS"
-  | "PT154"
-  | "VISCII"
-  | "ISO646-CN"
-  | "ISO646-JP"
-  | "HP-ROMAN8"
-  | "TIS620";
+  | "armscii8"
+  | "rk1048"
+  | "strk10482002"
+  | "tcvn"
+  | "tcvn5712"
+  | "tcvn57121"
+  | "georgianacademy"
+  | "georgianps"
+  | "pt154"
+  | "viscii"
+  | "iso646cn"
+  | "gb198880"
+  | "cn"
+  | "isoir57"
+  | "iso646jp"
+  | "isoir14"
+  | "csiso14jisc6220ro"
+  | "jisc62201969ro"
+  | "jp"
+  | "hproman8"
+  | "cshproman8"
+  | "r8"
+  | "roman8"
+  | "xroman8"
+  | "ibm1051"
+  | "macintosh"
+  | "mac"
+  | "csmacintosh"
+  | "ascii"
+  | "ascii8bit"
+  | "usascii"
+  | "ansix34"
+  | "ansix341968"
+  | "ansix341986"
+  | "csascii"
+  | "cp367"
+  | "ibm367"
+  | "isoir6"
+  | "iso646us"
+  | "iso646irv"
+  | "us"
+  | "tis620"
+  | "isoir166"
+  | "tis6200"
+  | "tis62025291"
+  | "tis62025330"
+  | "cp720"
+  | "mik";
 
 type SingleByteEncoding =
   | WindowsCodepage
@@ -128,16 +267,64 @@ type SingleByteEncoding =
 // --- Multi-byte Encodings ---
 
 // Japanese
-type JapaneseEncoding = "Shift_JIS" | "Windows-31j" | "Windows932" | "EUC-JP";
+type JapaneseEncoding =
+  | "shiftjis"
+  | "csshiftjis"
+  | "mskanji"
+  | "sjis"
+  | "windows31j"
+  | "ms31j"
+  | "xsjis"
+  | "windows932"
+  | "ms932"
+  | "932"
+  | "cp932"
+  | "eucjp";
 
 // Chinese
-type ChineseEncoding = "GB2312" | "GBK" | "GB18030" | "Windows936" | "EUC-CN";
+type ChineseEncoding =
+  | "gb2312"
+  | "gb231280"
+  | "gb23121980"
+  | "csgb2312"
+  | "csiso58gb231280"
+  | "euccn"
+  | "windows936"
+  | "ms936"
+  | "936"
+  | "cp936"
+  | "gbk"
+  | "xgbk"
+  | "isoir58"
+  | "gb18030"
+  | "chinese";
 
 // Korean
-type KoreanEncoding = "KS_C_5601" | "Windows949" | "EUC-KR";
+type KoreanEncoding =
+  | "windows949"
+  | "ms949"
+  | "949"
+  | "cp949"
+  | "cseuckr"
+  | "csksc56011987"
+  | "euckr"
+  | "isoir149"
+  | "korean"
+  | "ksc56011987"
+  | "ksc56011989"
+  | "ksc5601";
 
 // Taiwan/Hong Kong
-type TaiwanHongKongEncoding = "Big5" | "Big5-HKSCS" | "Windows950";
+type TaiwanHongKongEncoding =
+  | "windows950"
+  | "ms950"
+  | "950"
+  | "cp950"
+  | "big5"
+  | "big5hkscs"
+  | "cnbig5"
+  | "csbig5"
+  | "xxbig5";
 
 type MultiByteEncoding = JapaneseEncoding | ChineseEncoding | KoreanEncoding | TaiwanHongKongEncoding;
 
