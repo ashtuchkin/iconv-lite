429c8906b090763a026516f7da664541068d9239
Paul Ebose <paulebose@gmail.com>
2025-08-16 13:27:40 +0100

    make sure typings correctly describes usage in both cjs and esm

--- lib/index-template.d.ts
+++ lib/index-template.d.ts
@@ -11,33 +11,6 @@
 /** A union of all supported encoding strings in `iconv-lite`. */
 // --SUPPORTED-ENCODINGS-PLACEHOLDER--
 
-//   --- Basic API ---
-
-/** Decodes a `Buffer` into a `string`, using the provided `encoding`. */
-export function decode(buffer: Buffer | Uint8Array, encoding: SupportedEncoding, options?: DecodeOptions): string;
-
-/** Encodes a `string` into a `Buffer`, using the provided `encoding`. */
-export function encode(content: string, encoding: SupportedEncoding, options?: EncodeOptions): Buffer;
-
-/** Checks if a given encoding is supported by `iconv-lite`. */
-export function encodingExists(encoding: string): encoding is SupportedEncoding;
-
-//   --- Stream API ---
-
-/** Creates a stream that decodes binary data from a given `encoding` into strings. */
-export function decodeStream(encoding: SupportedEncoding, options?: DecodeOptions): NodeJS.ReadWriteStream;
-
-/** Creates a stream that encodes strings into binary data in a given `encoding`. */
-export function encodeStream(encoding: SupportedEncoding, options?: EncodeOptions): NodeJS.ReadWriteStream;
-
-//   --- Low-level stream APIs ---
-
-/** Creates and returns a low-level encoder stream. */
-export function getEncoder(encoding: SupportedEncoding, options?: EncodeOptions): EncoderStream;
-
-/** Creates and returns a low-level decoder stream. */
-export function getDecoder(encoding: SupportedEncoding, options?: DecodeOptions): DecoderStream;
-
 export interface DecodeOptions {
   /** Strips the byte order mark (BOM) from the input, when decoding. @default true */
   stripBOM?: boolean;
@@ -61,3 +34,34 @@ export interface DecoderStream {
   write(buf: Buffer): string;
   end(): string | undefined;
 }
+
+declare const iconv: {
+  //   --- Basic API ---
+
+  /** Decodes a `Buffer` into a `string`, using the provided `encoding`. */
+  decode(buffer: Buffer | Uint8Array, encoding: SupportedEncoding, options?: DecodeOptions): string;
+
+  /** Encodes a `string` into a `Buffer`, using the provided `encoding`. */
+  encode(content: string, encoding: SupportedEncoding, options?: EncodeOptions): Buffer;
+
+  /** Checks if a given encoding is supported by `iconv-lite`. */
+  encodingExists(encoding: string): encoding is SupportedEncoding;
+
+  //   --- Stream API ---
+
+  /** Creates a stream that decodes binary data from a given `encoding` into strings. */
+  decodeStream(encoding: SupportedEncoding, options?: DecodeOptions): NodeJS.ReadWriteStream;
+
+  /** Creates a stream that encodes strings into binary data in a given `encoding`. */
+  encodeStream(encoding: SupportedEncoding, options?: EncodeOptions): NodeJS.ReadWriteStream;
+
+  //   --- Low-level stream APIs ---
+
+  /** Creates and returns a low-level encoder stream. */
+  getEncoder(encoding: SupportedEncoding, options?: EncodeOptions): EncoderStream;
+
+  /** Creates and returns a low-level decoder stream. */
+  getDecoder(encoding: SupportedEncoding, options?: DecodeOptions): DecoderStream;
+};
+
+export default iconv;
