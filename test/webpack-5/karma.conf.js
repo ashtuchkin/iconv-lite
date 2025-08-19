e4f88cbe5971df27ed4f665bec943e623231c9b4
Sebastian Beltran <bjohansebas@gmail.com>
2025-08-18 20:16:09 -0500

    update webpack config
    Signed-off-by: Sebastian Beltran <bjohansebas@gmail.com>

--- test/webpack-5/karma.conf.js
+++ test/webpack-5/karma.conf.js
@@ -10,7 +10,12 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ["mocha"],
+    frameworks: ["mocha", "webpack"],
+
+    plugins: [
+      'karma-webpack',
+      'karma-mocha',
+    ],
 
     // list of files / patterns to load in the browser
     files: [
